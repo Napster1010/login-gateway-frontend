e5a1f21b6959927ac0dec0d578997ef56d50beb5
Napster1010 <aditya.ndps@gmail.com>
2021-10-31 15:08:42 +0530

    Update the app navigation activity when clicked on an app
    Signed-off-by: Napster1010 <aditya.ndps@gmail.com>

---
+++ src/app/data/services/user-activity-log/user-activity-log.service.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserActivityLogService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public updateAppNavigationActivity(appId: number): Observable<void> {
+    return this.httpClient.post<void>(`${environment.UPDATE_APP_NAVIGATION_ACTIVITY_API}/${appId}`, {});
+  }
+}
